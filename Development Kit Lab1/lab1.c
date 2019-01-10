73d3636721492c7a8438be2a89af3ef754a2433c
Tymoore Jamal <tymoore@ualberta.ca>
2019-01-10 14:57:46 -0700

    added arg handling

--- Development Kit Lab1/lab1.c
+++ Development Kit Lab1/lab1.c
@@ -3,7 +3,24 @@
 #include "lab1_IO.h"
 
 
-int main(){
+int main(int argc, char const *argv[])
+{
+    if (argc != 2){
+        printf("Invalid Number of Arguments\n");
+        exit(0);
+    }
+
+    int num_of_threads = atoi(argv[1]);
+
+    if (num_of_threads <= 0){
+        printf("Invalid argument");
+        exit(0);
+    }
+
+    int **A; int **B; int size;
+
+    Lab1_loadinput(&A, &B, &size);
+
 
     return 0;
 }
